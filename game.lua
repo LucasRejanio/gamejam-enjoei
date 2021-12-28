9b922acf4b2add1c7215eb8eae52d837d334ce01
Vinicius Santos <vinicius.santos@enjoei.com.br>
2021-12-28 18:24:29 -0300

    merge

--- game.lua
+++ game.lua
@@ -1,63 +1,68 @@
 game = {}
 
+scale = 3.5
+
 player = {
-  height = 32,
-  width = 32
+  height = 32 * scale,
+  width = 32 * scale,
+  velx = 0,
+  vely = 1
 }
 
 function game.load()
   background = love.graphics.newImage("img/backgrounds/background.png")
   player.image = love.graphics.newImage("img/players/bearSprite/bearSprite1.png")
 
-  player.x = 60
-  player.y = love.graphics.getHeight() - player.height - 55
+  player.x = 0
+  player.y = love.graphics.getHeight() - player.height - (55 * scale)
 end
 
 function game.draw()
-  love.graphics.draw(background, 0, 0)
+  love.graphics.draw(background, 0, 0, 0, scale, scale)
   player_width = player.width
   player_height = player.height
 
-  
   love.graphics.setColor(255 / 255, 255 / 255, 255 / 255)
-  love.graphics.rectangle('fill', player.x, player.y, player_width, player_height)
-  love.graphics.draw(player.image, player.x, player.y)
+  love.graphics.draw(player.image, player.x, player.y, 0, scale, scale)
   --love.graphics.draw(ball.image, ball.x, ball.y)
 end
 
-function love.update()
-  if love.keyboard.isDown('left') then
-    player.x = player.x - player.velx
+function game.update(dt)
+  maxY = love.graphics.getHeight() - player.height - (55 * scale) 
+
+  if love.keyboard.isDown("space") then
+    player.vely = -7
   end
 
-  if love.keyboard.isDown('right') then
-    player.x = player.x + player.velx
+  player.vely = player.vely + 0.1
+  player.y = player.y + player.vely
+
+  if player.y > maxY then
+    player.y = maxY
   end
 
-  if player.x < 0 then
-    player.x = 0
-  end
+  -- if player.x < 0 then
+  --   player.x = 0
+  -- end
 
-  if player.x + player.width > game.width then
-    player.x = game.width - player.width
-  end
+  -- if player.x + player.width > game.width then
+  --   player.x = game.width - player.width
+  -- end
 
-  ball.vely = ball.vely + 0.1
+  -- ball.vely = ball.vely + 0.1
 
-  ball.x = ball.x + ball.velx
-  ball.y = ball.y + ball.vely
+  -- ball.x = ball.x + ball.velx
+  -- ball.y = ball.y + ball.vely
 
-  -- bate na colisão
-  if checkCollision(ball, player) or ball.y + ball.height > game.height then
-    ball.vely = -7
-    hitSound:play()
-  end
+  -- -- bate na colisão
+  -- if checkCollision(ball, player) or ball.y + ball.height > game.height then
+  --   ball.vely = -7
+  --   hitSound:play()
+  -- end
 
-  -- bate nos cantos
-  if ball.x < 0 or ball.x + ball.width > game.width then
-    ball.velx = ball.velx * -1
-    hitSound:play()
-  end
-
+  -- -- bate nos cantos
+  -- if ball.x < 0 or ball.x + ball.width > game.width then
+  --   ball.velx = ball.velx * -1
+  --   hitSound:play()
+  -- end
 end
-
