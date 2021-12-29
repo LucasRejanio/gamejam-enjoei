1f6bd86b409d41b85d74141f4f01cb8651cca6db
Jordana Demori <jordana.picoloto@enjoei.com.br>
2021-12-29 16:25:26 -0300

    Add mouse sprite to instructions

--- how_to.lua
+++ how_to.lua
@@ -3,6 +3,7 @@ how_to = {}
 function how_to.load()
     font = love.graphics.newFont(32)
     how_to.spacebar_sprite = love.graphics.newImage("img/how_to/spacebar.png")
+    how_to.mouse_sprite = love.graphics.newImage("img/how_to/mouse.png")
     how_to.left_arrow = love.graphics.newImage("img/how_to/seta-esquerda.png")
     how_to.right_arrow = love.graphics.newImage("img/how_to/seta-direita.png")
 end
@@ -10,42 +11,48 @@ end
 function how_to.draw()
 
     draw_how_to_button(Game)
-    
+
     --spacebar sprite
     spacebar_scale = 4
     spacebar_x = ((Game.width*Game.scale)/2) - (how_to.spacebar_sprite:getWidth()*spacebar_scale/2)
-    spacebar_y = ((Game.height*Game.scale)/2)
+    spacebar_y = ((Game.height*Game.scale)/2.3)
     love.graphics.draw(how_to.spacebar_sprite, spacebar_x,spacebar_y,0,spacebar_scale,spacebar_scale)
 
     --arrows sprites
     arrow_scale = 4
     left_arrow_x = ((Game.width*Game.scale)/3) - (how_to.left_arrow:getWidth()*arrow_scale/2)
-    left_arrow_y = ((Game.height*Game.scale)/5)
+    left_arrow_y = ((Game.height*Game.scale)/5.5)
     love.graphics.draw(how_to.left_arrow, left_arrow_x,left_arrow_y,0,arrow_scale,arrow_scale)
 
     right_arrow_x = ((Game.width*Game.scale)*2/3) - (how_to.right_arrow:getWidth()*arrow_scale/2)
-    right_arrow_y = ((Game.height*Game.scale)/5)
+    right_arrow_y = ((Game.height*Game.scale)/5.5)
     love.graphics.draw(how_to.right_arrow, right_arrow_x,right_arrow_y,0,arrow_scale,arrow_scale)
+
+    --mouse sprite
+    mouse_scale = 2
+    mouse_x = ((Game.width*Game.scale)/1.9) - (how_to.spacebar_sprite:getWidth()*spacebar_scale/1.9)
+    mouse_y = ((Game.height*Game.scale)/1.5)
+    love.graphics.draw(how_to.mouse_sprite, mouse_x, mouse_y,0, mouse_scale, mouse_scale)
 
     --spacebar text
     spacebar_text = love.graphics.newText(love.graphics.newFont(30),"Pressione a barra de espaço para pular")
     spacebar_text_x = ((Game.width*Game.scale)/2) - (spacebar_text:getWidth()/2)
-    spacebar_text_y = ((Game.height*Game.scale)/2) - how_to.spacebar_sprite:getHeight()*spacebar_scale
+    spacebar_text_y = ((Game.height*Game.scale)/2.5) - how_to.spacebar_sprite:getHeight()*spacebar_scale
 
     --arrows text
-    arrows_text = love.graphics.newText(love.graphics.newFont(30),"Use as setas para mudar de personagem")
+    arrows_text = love.graphics.newText(love.graphics.newFont(30),"Use as setas do teclado para mudar de personagem")
     arrows_text_x = ((Game.width*Game.scale)/2) - (arrows_text:getWidth()/2)
-    arrows_text_y = ((Game.height*Game.scale)/5) - how_to.left_arrow:getHeight()*arrow_scale
+    arrows_text_y = ((Game.height*Game.scale)/7) - how_to.left_arrow:getHeight()*arrow_scale
 
     --mouse text
     mouse_text = love.graphics.newText(love.graphics.newFont(30),"O resto é com o mouse :P")
     mouse_text_x = ((Game.width*Game.scale)/2) - (mouse_text:getWidth()/2)
-    mouse_text_y = ((Game.height*Game.scale)*3/5)
+    mouse_text_y = ((Game.height*Game.scale)*3/5.2)
 
      --how_to text
-     how_to_text = love.graphics.newText(love.graphics.newFont(30),"E lembre, não chegue perto dos bugs")
+     how_to_text = love.graphics.newText(love.graphics.newFont(30),"E lembre-se: não chegue perto dos bugs!")
      how_to_text_x = ((Game.width*Game.scale)/2) - (how_to_text:getWidth()/2)
-     how_to_text_y = ((Game.height*Game.scale)*4/5)
+     how_to_text_y = ((Game.height*Game.scale)*4/5.1)
 
     love.graphics.setColor(0,0,0)
     love.graphics.draw(spacebar_text,spacebar_text_x,spacebar_text_y)
@@ -71,15 +78,15 @@ function draw_how_to_button(Game)
         last = false,
         now = false
     }
-    
+
     button.last = button.now
 
     local bx = (ww * 0.5) - (button.width * 0.5)
-    local by = (wh) - (button.height * 1.1)
+    local by = (wh) - (button.height * 1.9)
 
     local button_color = {1.0, 0.83, 0.87, 1}
 
-    local mx, my = love.mouse.getPosition() 
+    local mx, my = love.mouse.getPosition()
 
     local hot = mx > bx and mx < bx + button.width and
                 my > by and my < by + button.height
