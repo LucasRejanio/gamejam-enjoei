f4412734c2a29d3a718765d43ca47d7a057e8c7d
Marcos Angeli <marcos.angeli@enjoei.com.br>
2021-12-29 10:27:15 -0300

    score and lives feature

--- game.lua
+++ game.lua
@@ -1,4 +1,7 @@
-game = {}
+game = {
+  current_score = 0,
+  current_lives = 3
+}
 
 scale = 3.5
 
@@ -38,7 +41,15 @@ function game.draw()
 
   love.graphics.setColor(255 / 255, 255 / 255, 255 / 255)
   love.graphics.draw(avatar_frames[Game.selected_avatar][math.floor(player.avatar_current_frame)], player.x, player.y, 0, scale, scale)
+
+  --Score e Pontuação
+  love.graphics.setColor(0,0,0)
+  love.graphics.print("pontuação: " .. game.current_score,0,90,0,2,2)
+  love.graphics.print("vidas: " .. game.current_lives,0,110,0,2,2)
+  love.graphics.setColor(1,1,1)
 end
+
+spacePressed = false
 
 function game.update(dt)
   player.avatar_current_frame = player.avatar_current_frame + 10 * dt
@@ -60,7 +71,6 @@ function game.update(dt)
     player.y = maxY
     timer_counter = 0
   end
-
   -- if player.x < 0 then
   --   player.x = 0
   -- end
@@ -85,4 +95,19 @@ function game.update(dt)
   --   ball.velx = ball.velx * -1
   --   hitSound:play()
   -- end
+end
+
+--Atualiza as vidas e pontuaçao se houve colisão
+function object_collided(isBug) 
+  --Se for bug
+  if isBug then
+    game.current_lives = game.current_lives - 1
+    if game.current_lives == 0 then
+      Game.scene = "game_over"
+    end
+
+  --Se nao for bug
+  else 
+    game.current_score = game.current_score + 1
+  end
 end
